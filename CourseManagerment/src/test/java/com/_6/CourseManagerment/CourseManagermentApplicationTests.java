c87e71e45c020f222914ffe8ff8742f4d9ea3af5
JinNguyen-superstar <Phucnguyenn0608@gmail.com>
2026-03-22 12:40:31 +0700

    Add depencies

---
+++ CourseManagerment/src/test/java/com/_6/CourseManagerment/CourseManagermentApplicationTests.java
@@ -0,0 +1,13 @@
+package com._6.CourseManagerment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagermentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
